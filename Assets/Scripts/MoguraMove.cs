9a97bca928d272b1ba204e5a16cb93895144a571
nobutoraMori <gasutorageta@gmail.com>
2025-03-09 21:05:56 +0900

    isHitフラグを用意してフラグがある状態では叩けないように設定 アニメーションが終わったら叩けるようにする

--- Assets/Scripts/MoguraMove.cs
+++ Assets/Scripts/MoguraMove.cs
@@ -8,34 +8,32 @@ public class Mole : MonoBehaviour
     private Animator animator;
     private Collider2D moleCollider;
     private Image imageComponent;
-    public List<AnimationClip> damageAnimations;
     public List<Sprite> moleSprites;
 
-    private AudioSource audioSource; // ���̍s��ǉ�
-    public List<AudioClip> audioClips; // ���̍s��ǉ�
-    
-    public 
+    private AudioSource audioSource;
+    public List<AudioClip> audioClips;
+
+    private bool isHit = false; // 連続で叩かれるのを防ぐフラグ
+
     void Start()
     {
         animator = GetComponent<Animator>();
         moleCollider = GetComponent<Collider2D>();
         imageComponent = GetComponent<Image>();
-        audioSource = GetComponent<AudioSource>(); // ���̍s��ǉ�
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Hummer"))
+        if (other.CompareTag("Hummer") && !isHit)
         {
-            Debug.Log("�Փ˂�������");
-            // �X�e�[�g1����X�e�[�g2�ւ̑J��
-            animator.SetBool("MemeReturn", true);
-            Debug.Log("MemeReturn��true�ɐݒ�");
+            isHit = true; // 叩かれたことを記録
+            Debug.Log("モグラが叩かれた！");
 
-            // �����_���ȉ����N���b�v���Đ�
-            PlayRandomAudioClip(); // ���̍s��ǉ�
-            
-            // �_���[�W�������Ă�
+            animator.SetBool("MemeReturn", true);
+            Debug.Log("MemeReturn を true に設定");
+
+            PlayRandomAudioClip();
             TakeDamage();
         }
     }
@@ -44,19 +42,19 @@ public class Mole : MonoBehaviour
     {
         ScoreManager.Instance.AddScore(1);
 
-        animator.SetTrigger("MemeFreeze"); // �X�e�[�g2����X�e�[�g3�ւ̑J��
-
-        StartCoroutine(ResetMemeReturn());//�񓯊������X�^�[�g
+        animator.SetTrigger("MemeFreeze");
+        StartCoroutine(WaitForAnimationAndReset("MemeFreeze")); // 修正：アニメーションが終わるのを待ってリセット
     }
 
-    private IEnumerator ResetMemeReturn()//�񓯊�����
+    private IEnumerator WaitForAnimationAndReset(string animationName)
     {
-        // 3�ڂ̃A�j���[�V�������I��������Ƃ�҂�
-        yield return new WaitForSeconds(1.5f); // �K�؂Ȏ��Ԃɒ���
-        animator.SetBool("MemeReturn", false); // �X�e�[�g3����X�e�[�g4�ւ̑J��
+        yield return new WaitForSeconds(1.5f); // 少し待機してからリセット
+        
+        animator.SetBool("MemeReturn", false);
+        isHit = false; // 叩かれるのを解除
         MoleAnimater();
         UpdateSprite();
-        Debug.Log("MemeReturn��false�ɐݒ�");
+        Debug.Log("MemeReturn を false に設定");
     }
 
     private void UpdateSprite()
@@ -65,17 +63,15 @@ public class Mole : MonoBehaviour
         {
             int randomIndex = Random.Range(0, moleSprites.Count);
             imageComponent.sprite = moleSprites[randomIndex];
-           
         }
     }
 
     private void MoleAnimater()
     {
-        // MoleAnimatorAssigner���擾���ă��\�b�h���Ăяo��
         MoleAnimatorAssigner animatorAssigner = GetComponent<MoleAnimatorAssigner>();
         if (animatorAssigner != null)
         {
-            animatorAssigner.AssignRandomAnimator(); // �����_���ȃA�j���[�^�[�����蓖�Ă�
+            animatorAssigner.AssignRandomAnimator();
         }
     }
 
